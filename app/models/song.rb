7fbff121c60fc290861106b2ef2fbace0d549ebd
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-13 08:45:10 -0700

    Add error message when song field not valid

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,5 @@
 class Song < ApplicationRecord
   belongs_to :album
+
+  validates :name, presence: true
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe Song do
   it { should belong_to(:album) }
+  it { should validate_presence_of :name}
 end
